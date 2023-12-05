a2aea364a5be126ae24931eaa62e1dd8840afed9
Frosso Varsou <frossovarsou@gmail.com>
2023-12-05 15:37:20 +0200

    Update Ex1.c

--- MPI/Ex1.c
+++ MPI/Ex1.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include <string.h>
+#include <math.h>
+#include <time.h>
 #include <mpi.h>
 
-float print_VAL(float m,char msg[]);
+// Συναρτήσεις που συλλέγει ο κεντρικός επεξεργαστής και τυπώνει τα αποτελέσματα
+float print_VAL(float m, char msg[]);
 float print_MAX(int max);
-void print_D(int d[]);
+void print_D(int* d);
 
+// global μεταβλητές που χρησιμοποιουν όλες οι διεργασίες (και οι συναρτήσεις)
 int rank, p, n, num;
 int tag1 = 50, tag2 = 60, tag3 = 70;
 MPI_Status status;
 
 int main(int argc, char **argv) 
 {
-    int i,target, choice;
-    int x[100], x_loc[100];
-    char msg[50];
+    int i, target, choice;
+    int *x, *x_loc;
+    char msg[20];
+    srand(time(NULL));
 
     MPI_Init(&argc, &argv);                // Αρχικοποιήση MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Τρέχουσα process id
@@ -29,8 +36,12 @@ int main(int argc, char **argv)
                 printf("Δώσε πλήθος n: ");
                 scanf("%d", &n);
             } 
-            while(n%p != 0 || n>=100);  //έλεγχος για αν n ακέραιο πολλαπλάσιο του p
+            while(n%p != 0);  //έλεγχος για αν n ακέραιο πολλαπλάσιο του p
 
+            // Δέσμευση πίνακα για n στοιχεία
+            x = (int *) malloc(n * sizeof(int)); assert(x != NULL);
+
+            // Εισαγωγή των στοιχείων του πίνακα
             printf("Δώσε τα %d στοιχεία : ", n);
             for(i = 0; i < n; i++)
                 scanf("%d", &x[i]);
@@ -46,14 +57,18 @@ int main(int argc, char **argv)
                 MPI_Send(&x[i], num, MPI_INT, target, tag2, MPI_COMM_WORLD);
                 i += num;
             }
-            
+
+            // Δέσμευση x_loc για τον κεντρικό επεξεργαστή
+            x_loc = (int *) malloc(num * sizeof(int)); assert(x_loc != NULL);
             for(i = 0; i < num; i++)
-                x_loc[i]=x[i];
+                x_loc[i] = x[i];
         }
         else // άλλοι επεξεργαστές
         {
             MPI_Recv(&n, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, &status);
-            num = n/p;
+            num = n/p; // πλήθος θέσεων του x_loc
+            // Δέσμευση x_loc για τις υπόλοιπες διεργασίες
+            x_loc = (int *) malloc(num * sizeof(int)); assert(x_loc != NULL);
             MPI_Recv(&x_loc[0], num, MPI_INT, 0, tag2, MPI_COMM_WORLD, &status);
         }
 
@@ -80,7 +95,7 @@ int main(int argc, char **argv)
         else  // Ο Κεντρικός επεξεργαστής
         { 
             // (α) Υπολογισμός συνολικού μέσου όρου
-            strcpy(msg,"Μέσος Όρος");
+            strcpy(msg,"Μέσος Όρος"); 
             mo = print_VAL(mo,msg);       
             // (β) Υπολογισμός συνολικού μέγιστου
             max = print_MAX(max);  
@@ -94,11 +109,20 @@ int main(int argc, char **argv)
         
         // Υπολογισμός διασποράς (variance) και διανύσματος d
         float var = 0;
-        int d_loc[100];
+        int* d_loc;
+        if(rank == 0)
+        {
+            d_loc = (int *) malloc(n * sizeof(int)); assert(d_loc != NULL);
+        }
+        else 
+        {
+            d_loc = (int *) malloc(num * sizeof(int)); assert(d_loc != NULL);
+        }
+           
         for(i = 0; i < num; i++)
         {
             var += (x_loc[i] - mo)*(x_loc[i] - mo);
-            d_loc[i] = (x_loc[i] - max)*(x_loc[i] - max);
+            d_loc[i] = fabs((x_loc[i] - max)*(x_loc[i] - max));
         }    
         var = var/n;
         
@@ -109,6 +133,9 @@ int main(int argc, char **argv)
             MPI_Send(&d_loc[0], num, MPI_INT, 0, tag3, MPI_COMM_WORLD);
             // Λαμβάνουν απο τον 0 την επιλογή του χρήστη
             MPI_Recv(&choice, 1, MPI_INT, 0, tag3, MPI_COMM_WORLD,&status);
+            // Αποδεσμευση μνήμης για τις υπόλοιπες διεργασίες
+            free(x_loc);
+            free(d_loc);
         }
         else // Ο Κεντρικός επεξεργαστής
         {
@@ -120,12 +147,20 @@ int main(int argc, char **argv)
 
             printf("\n------------MENOY------------\n");
             printf("\t1. Συνέχεια\n\t2. Έξοδος\n");
-            printf("Επιλογή: ");
-            scanf("%d", &choice);
+            do
+            {
+                printf("Επιλογή: ");
+                scanf("%d", &choice);
+            }
+            while(choice != 1 && choice != 2);
             printf("-----------------------------\n\n");
             // Στέλνει σε όλους την επιλογή του χρήστη
             for (target = 1; target < p; target++) 
                 MPI_Send(&choice, 1, MPI_INT, target, tag3, MPI_COMM_WORLD); 
+            // Αποδεσμευση μνήμης για τον κεντρικό επεξεργαστή
+            free(x); 
+            free(x_loc);
+            free(d_loc);
         }
     }
     while(choice != 2);
@@ -167,7 +202,7 @@ float print_MAX(int max)
     return finmax;
 }
 
-void print_D(int d[])
+void print_D(int* d)
 {       
     int source; 
     int i = num;
